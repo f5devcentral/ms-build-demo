7cd8f2b68a36b481f7dcf8a4d7191d6d0bdc7cdf
Kevin Reynolds <k.reynolds@f5.com>
2023-04-24 08:25:51 -0400

    updated OAS

--- loadgen/baseline/main.js
+++ loadgen/baseline/main.js
@@ -1,9 +1,7 @@
-import { sleep } from "k6";
-
-import { crawler } from "./crawler.js";
-import { bot } from "./bot.js";
-import { exploit } from "./exploit.js";
-import { synthetic } from "./synthetic.js";
+import { sleep, check } from "k6";
+import http from 'k6/http';
+import { SharedArray } from 'k6/data';
+import { addRandAgent } from './helpers.js';
 
 export const options = {
     stages: [
@@ -11,18 +9,36 @@ export const options = {
     ]
   };
 
-const vars = [];
+const data = new SharedArray('data', function () {
+  const f = JSON.parse(open('./data.json'));
+  return f;
+});
 
-const isDebug = false;
-
-function getRandom(min, max) {
+function getRandomNum(min, max) {
     return Math.random() * (max - min) + min;
   }
 
-export default function main() {
-  exploit();
-  synthetic();
-  crawler();
-  bot();
-  sleep(getRandom(2, 5));
+function checkRes(res) {
+  check(res, {
+      'status is 200': (r) => r.status === 200
+  });
+}
+
+export default function () {
+  const base = `${__ENV.TARGET_URL}`
+  const randomPath = data[Math.floor(Math.random() * data.length)];
+  const url = base + randomPath.path
+  let res = http.get(url, addRandAgent());
+  checkRes(res)
+  if (randomPath.post) {
+    const value = randomPath.values[Math.floor(Math.random() * randomPath.values.length)];
+    let data = JSON.stringify({
+      value: value.name
+    });
+    let res = http.post(url, data, {
+      headers: { 'Content-Type': 'application/json' }},
+      addRandAgent());
+    checkRes(res)
+  }
+  sleep(getRandomNum(1, 5));
 }
